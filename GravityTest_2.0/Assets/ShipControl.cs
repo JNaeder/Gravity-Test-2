56d5808cd97608c1d59a1961bddc768dd81209ec
JNaeder <j.naeder324@gmail.com>
2020-07-14 17:45:05 -0400

    small again

--- GravityTest_2.0/Assets/ShipControl.cs
+++ GravityTest_2.0/Assets/ShipControl.cs
@@ -38,6 +38,8 @@ public class ShipControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         if (shipControlStatus)
         {
             rb.gravityScale = 1;
